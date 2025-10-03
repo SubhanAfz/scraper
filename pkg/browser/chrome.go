435f8cfabf7c8c596f8d2746c5c36569e356fdcb
SubhanAfz <subhanafzal125@gmail.com>
2025-10-03 14:11:55 +0100

    Change so headless is on

--- pkg/browser/chrome.go
+++ pkg/browser/chrome.go
@@ -17,7 +17,7 @@ type Chrome struct {
 
 func NewChrome() (*Chrome, error) {
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("headless", false),
+		chromedp.Flag("headless", true),
 		chromedp.UserAgent("'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'"),
 	)
 
